8e09ad04282de38170be81fee8ae1dfa74d6a69c
Daniel-R-Mott <daniel.r.mott@gmail.com>
2024-10-01 11:43:04 -0700

    added persistence

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import React, { Component } from "react";
 import NameTagList from "./NameTagList.js";
 import UserInput from "./UserInput.js";
 
+
+
 class App extends Component {
   state = {
     names: ["Erin", "Ann", "Nichole", "Sharon", "Maryn"]
   };
+
+  componentDidUpdate() {
+    const savedNamesString = JSON.stringify(this.state.names);
+    localStorage.setItem("savedNames", savedNamesString);
+}
+
+componentDidMount() {
+    const savedNamesString = localStorage.getItem("savedNames");
+    if (savedNamesString) {
+        const savedNames = JSON.parse(savedNamesString);
+        this.setState({ names: savedNames });
+    }
+}
+
   addName = (name) => {
     const newNames = [name, ...this.state.names];
     this.setState({ names: newNames });
